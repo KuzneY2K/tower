837c1d396337a7bf963b144f82504b144a954c39
Mark Kuznetsov <mkuz.fl@gmail.com>
2023-09-20 13:15:28 -0600

    .

--- Tower/src/services/CommentsService.js
+++ Tower/src/services/CommentsService.js
@@ -5,6 +5,7 @@ class CommentsService {
         const newComment = await dbContext.Comments.create(commentBody)
         await newComment.populate('creator', 'name picture id _id')
         await newComment.populate('event', 'name id _id')
+        return newComment
     }
 
 }

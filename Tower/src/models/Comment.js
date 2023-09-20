5aff4ea5935609ff24db08c415a2ebd97878de51
Mark Kuznetsov <mkuz.fl@gmail.com>
2023-09-20 13:17:39 -0600

    .

--- Tower/src/models/Comment.js
+++ Tower/src/models/Comment.js
@@ -4,7 +4,7 @@ export const CommentSchema = new Schema({
     creatorId: { type: Schema.Types.ObjectId, ref: 'Account', required: true },
     eventId: { type: Schema.Types.ObjectId, ref: 'Event', required: true },
     body: { type: String, required: true, maxlength: 500, minlength: 1 },
-    isAttending: { type: Boolean, required: true }
+    isAttending: { type: Boolean, }
 })
 
 CommentSchema.virtual('creator', {

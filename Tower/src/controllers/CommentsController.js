42a9b642a5e9e6e8a2f2b23adcedcab38c5a5354
Mark Kuznetsov <mkuz.fl@gmail.com>
2023-09-20 13:15:18 -0600

    .

--- Tower/src/controllers/CommentsController.js
+++ Tower/src/controllers/CommentsController.js
@@ -1,8 +1,22 @@
+import { Auth0Provider } from "@bcwdev/auth0provider";
 import BaseController from "../utils/BaseController.js";
+import { commentsService } from "../services/CommentsService.js";
 
 export class CommentsController extends BaseController {
     constructor() {
         super('api/comments')
         this.router
+            .use(Auth0Provider.getAuthorizedUserInfo)
+            .post('', this.createComment)
+    }
+    async createComment(req, res, next) {
+        try {
+            const commentBody = req.body
+            commentBody.creatorId = req.userInfo.id
+            const newComment = await commentsService.createComment(commentBody)
+            res.send(newComment)
+        } catch (error) {
+            next(error)
+        }
     }
 }
--- Tower/src/services/CommentsService.js
+++ Tower/src/services/CommentsService.js
@@ -1,4 +1,11 @@
+import { dbContext } from "../db/DbContext.js"
+
 class CommentsService {
+    async createComment(commentBody) {
+        const newComment = await dbContext.Comments.create(commentBody)
+        await newComment.populate('creator', 'name picture id _id')
+        await newComment.populate('event', 'name id _id')
+    }
 
 }
 
